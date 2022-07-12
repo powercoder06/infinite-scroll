403846b9300440a7cecae557bddfc0695148e288
powercoder06 <ayush.kumar2021c@vitstudent.ac.in>
2022-07-13 02:30:49 +0530

    infinte scrolling added

--- script.js
+++ script.js
@@ -5,24 +5,32 @@ let photosArray= [];
 
 // Unsplash API
 const count= 10;
-const apiKey ='egiD9Poc5zK3VVMtbRzo723zkBny1vPF8t_bc3AWGpE';
+const apiKey ='i3fQdOboRWO6AxKTZpQZd8l7rwayZ0DNycswq2kwYbo';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Helper Function to set attributes on DOM Elements
-
+function setAttribute(Element, attributes) {
+    for(const key in attributes){
+        Element.setAttribute(key, attributes[key]);
+    }
+}
 
 // Create Elements for links photos and add to DOM
 function displayPhotos() {
     photosArray.forEach((photo) =>{
         //create <a> to link to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttribute(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         // Create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title',photo.alt_description);
+        setAttribute(img,{
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
         // Put <img> inside <a> then put both inside image container Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -42,4 +50,14 @@ async function getPhotos(){
 
     }
 }
+
+// schecking if scrolli ng enables
+window.addEventListener('scroll', ()=> {
+    if(window.innerHeight+ window.scrollY >= document.body.offsetHeight -1000)
+    {
+        getPhotos();
+        console.log('load more');
+    }
+});
+
 getPhotos();
